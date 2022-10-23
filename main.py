2fc904e857764e3656b48b6435c646b0141792af
Манчук Никита Александрович <n.manchuk@okbsapr.ru>
2022-10-24 01:35:12 +0300

    Add RSA sign

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
 
 
-def solve_linear_congruence(a, b, m) -> int:
+def solve_linear_congruence(a, b, m) -> (int, int):
     g = math.gcd(a, m)
 
     if b % g:
@@ -12,7 +12,7 @@ def solve_linear_congruence(a, b, m) -> int:
     return pow(a, -1, m) * b % m, m
 
 
-def solve_congruence(a, b, m, var_name = "x") -> (str, int):
+def solve_congruence(a, b, m, var_name="x") -> (str, int):
     """Solve ax = b mod m"""
 
     res = f"Решение сравнения: {a} * {var_name} = {b} mod {m}\n"
@@ -57,7 +57,7 @@ def quick_pow(base, exp, mod) -> (str, int):
     return res, a
 
 
-def rsa_encrypt(m = None, e = None, d = None, n = None, p = None, q = None, fn = None) -> (str, int):
+def rsa_encrypt(m=None, e=None, d=None, n=None, p=None, q=None, fn=None) -> (str, int):
     res = f"Шифрование данных алгоритмом RSA:\n"
     c = None
 
@@ -115,6 +115,64 @@ def rsa_decrypt(c=None, e=None, d=None, n=None, p=None, q=None, fn=None) -> (str
     return res, m
 
 
+def rsa_sign(m=None, e=None, d=None, n=None, p=None, q=None, fn=None) -> (str, int):
+    res = f"Шифрование данных алгоритмом RSA:\n"
+    c = None
+
+    try:
+        if n is None:
+            n = p * q
+            res += f"n = p * q = {p} * {q} = {n}\n"
+
+        if d is None:
+            #if e is None: #(e, fn) = 1 TODO
+
+            if fn is None:
+                fn = (p - 1) * (q - 1)
+                res += f"ф(n) = (p-1)*(q-1) = {p - 1}*{q - 1} = {fn}\n"
+
+            s, d = solve_congruence(e, 1, fn, "d")
+            res += s
+
+        s, c = quick_pow(m, d, n)
+        res += s
+    except TypeError:
+        res += "Недостаточно данных\n"
+    else:
+        res += f"s = m^d mod n = {m}^{d} mod {n} = {c} mod {n}\n"
+
+    return res, c
+
+
+def rsa_check_sign(c=None, e=None, d=None, n=None, p=None, q=None, fn=None) -> (str, int):
+    res = f"Расифрование данных алгоритмом RSA:\n"
+    m = None
+
+    try:
+        if n is None:
+            n = p * q
+            res += f"n = p * q = {p} * {q} = {n}\n"
+
+        if e is None:
+            # if d is None: #(e, fn) = 1 TODO
+
+            if fn is None:
+                fn = (p - 1) * (q - 1)
+                res += f"ф(n) = (p-1)*(q-1) = {p - 1}*{q - 1} = {fn}\n"
+
+            s, e = solve_congruence(d, 1, fn, "e")
+            res += s
+
+        s, m = quick_pow(c, e, n)
+        res += s
+    except TypeError:
+        res += "Недостаточно данных\n"
+    else:
+        res += f"m\' = s^e mod n = {c}^{e} mod {n} = {m} mod {n}\n"
+
+    return res, m
+
+
 def test():
     output, num = quick_pow(175, 235, 257)
     print(output)
@@ -123,6 +181,10 @@ def test():
     output, num = rsa_encrypt(p=7, q=11, m=29, d=7)
     print(output)
     output, num = rsa_decrypt(p=7, q=11, e=43, c=19)
+    print(output)
+    output, num = rsa_sign(p=5, q=11, d=3, m=40)
+    print(output)
+    output, num = rsa_check_sign(p=5, q=11, d=3, c=35)
     print(output)
 
 
